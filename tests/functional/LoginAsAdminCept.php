b2c6c3f59a4ccfd82ef638a520622cb760d9e124
Spencer Mandzik <spencer.mandzik@gmail.com>
2017-07-25 13:23:41 -0700

    Simplified functional test for review test

--- tests/functional/LoginAsAdminCept.php
+++ tests/functional/LoginAsAdminCept.php
@@ -8,12 +8,7 @@
 
 
 $I = new FunctionalTester($scenario);
-$I->wantTo("Add a post as an admin");
+$I->wantTo("Login as an admin");
 $I->loginAsAdmin();
-$I->amOnPage('/wp-admin/edit.php');
-$I->click('Add New');
-$I->fillField('#title','Title of Post');
-$I->fillField('#content','Sample Text inside body element');
-$I->amGoingTo('submit a post as a admin');
-$I->click( 'Publish' );
+
 
