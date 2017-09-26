186aacaa105b0ca5a5e3e9c0a023a11600e430de
Spencer Mandzik <spencer.mandzik@gmail.com>
2017-09-26 16:14:07 -0700

    renamed root folder, removed cachee folders, modified gitignore

--- content/themes/chappress/functions.php
+++ content/themes/chappress/functions.php
@@ -34,12 +34,8 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_login_logo() { ?>
     <style type="text/css">
         #login h1 a, .login h1 a {
-            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/site-login-logo.svg);
-            height:65px;
-            width:320px;
-            background-size: 320px 65px;
+            background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/images/cu-logo.png);
             background-repeat: no-repeat;
-            padding-bottom: 30px;
         }
     </style>
 <?php }
