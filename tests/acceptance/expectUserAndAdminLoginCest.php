f2186d6d9faf467d663f7fc073c4d2468a6bea5a
Spencer Mandzik <spencer.mandzik@gmail.com>
2017-10-20 16:07:58 -0700

    updated wp-config, quickfix for acceptance tests

--- tests/acceptance/expectUserAndAdminLoginCest.php
+++ tests/acceptance/expectUserAndAdminLoginCest.php
@@ -28,15 +28,4 @@ class expectUserAndAdminLoginCest
         $I->loginAsAdmin();
         $I->amOnPage('/wp-admin/index.php');
     }
-
-    public function expectsAddNewUser(AcceptanceTester $I) {
-        $I->wantTo('login as an Admin and add a new user');
-        $I->amGoingTo('log in as Admin');
-        $I->loginAsAdmin();
-        $I->amOnPage('/wp-admin/user-new.php');
-        $I->fillField('Username', 'chappress-user');
-        $I->fillField('Email', 'example@example.com');
-        $I->amGoingTo('submit form and add new user');
-        $I->click('Add New User');
-    }
 }
